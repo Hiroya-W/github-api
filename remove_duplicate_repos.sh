bf6837a74c373422c20e9dcc2e500b6d49e687b9
Hiroya-W <hyuyukun@gmail.com>
2021-10-16 22:29:16 +0900

    :+1: リポジトリを持たないユーザディレクトリを削除する

--- remove_duplicate_repos.sh
+++ remove_duplicate_repos.sh
@@ -30,4 +30,18 @@ cat "$HASH_LIST_DUP_FILE" | while read -r line; do
     fi
 done
 
+DIRNAME=$(dirname $HASH_LIST_DUP_FILE)
+
+# 中身が空のユーザディレクトリを削除する
+ls "$DIRNAME/github.com" | while read -r line; do
+    if [ -z "$(ls $DIRNAME/github.com/$line)" ]; then
+        echo "rmdir $DIRNAME/github.com/$line"
+        rmdir "$DIRNAME/github.com/$line"
+    else
+        echo "$(ls $DIRNAME/github.com/$line)" | while read -r line2; do
+            echo "$line/$line2"
+        done
+    fi
+done
+
 exit 0
