424468d4bffe36257d16565a9897f4c5dbc3ef7a
Hiroya-W <hyuyukun@gmail.com>
2021-10-18 17:12:50 +0900

    :sparkles: コード検索するメソッドを用意

--- experiments/github/github.py
+++ experiments/github/github.py
@@ -1,9 +1,14 @@
+import urllib.parse
+from typing import Optional
+
 from requests.models import Response
 
 from .session import SessionCore
 
 
 class GitHubCore:
+    BASEURL = "https://api.github.com"
+
     def __init__(self) -> None:
         sc = SessionCore()
         self._session = sc.session
@@ -13,5 +18,35 @@ class GitHubCore:
 
 
 class GitHub(GitHubCore):
-    def run(self) -> Response:
-        return self._session.get("https://api.github.com/search/code?q=keyword")
+    def search_code(
+        self,
+        query: str,
+        per_page: Optional[int] = None,
+        page: Optional[int] = None,
+        text_match: bool = False,
+    ) -> Response:
+        ENDPOINT = "/search/code"
+        headers = {"Accept": "application/vnd.github.v3+json"}
+
+        params = _search_code_query_builder(query, per_page, page)
+
+        if text_match:
+            headers["Accept"] = "application/vnd.github.v3.text-match+json"
+
+        return self._session.get(
+            self.BASEURL + ENDPOINT, params=params, headers=headers
+        )
+
+
+def _search_code_query_builder(
+    query: str,
+    per_page: Optional[int] = None,
+    page: Optional[int] = None,
+) -> str:
+    params = {"q": query}
+    if per_page is not None:
+        params["per_page"] = str(per_page)
+    if page is not None:
+        params["page"] = str(page)
+
+    return urllib.parse.urlencode(params, safe=":")
